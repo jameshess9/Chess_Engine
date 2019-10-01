239e90df8ba3f9df110c21c515f33ba83108ea3e
surface <jshess@buffalo.edu>
2019-09-30 21:17:22 -0400

    commit and push from surface

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "The beginnings of a chess engine" << std::endl;
+    std::cout << "The beginnings of a chess engine starts one step at a time" << std::endl;
     return 0;
 }
